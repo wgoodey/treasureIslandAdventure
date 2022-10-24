77d2ce3f4a8da4a68d755a681ee1bc724dc60780
Whitney Goodey <wgoodey@gmail.com>
2022-10-24 10:34:46 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+
+response = input('Where do you want to go? Type "left" or "right"\n').lower()
+if response == "left":
+    response = input('There\'s a small building with a moat around it. Will you swim across or wait for a boat? Type '
+                     '"swim" or "wait".\n').lower()
+    if response == "wait":
+        response = input('You looked around while you waited and found some materials to make a raft. On the other '
+                         'side you see the building has three doors. Which one will you use? Type "red", "yellow", '
+                         'or "blue" n').lower()
+        if response == "yellow":
+            print("You found the treasure! How will you spend it?")
+        else:
+            print("That was the wrong door. A cassowary kicked you to death.\nGame Over")
+    else:
+        print("You were eaten by an alligator.\nGame Over")
+else:
+    print("You fell into a pit and died.\nGame Over")
+
